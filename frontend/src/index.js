cf8a703a40204cce3362cf5205b163829988ee86
Nkongchou Fomenky <nfomenky@student.ysu.edu>
2021-02-26 16:28:19 -0600

    Setup redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store';
 import './index.css';
 import App from './App';
 
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+const Root = () => {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  )
+}
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Root />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
