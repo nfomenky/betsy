f89d3a8408688fea212091032516fa7718d8b2bc
Nkongchou Fomenky <nfomenky@student.ysu.edu>
2021-02-26 11:12:07 -0600

    Added backend signup api route

---
+++ backend/db/seeders/20210224204651-demo-user.js
@@ -0,0 +1,44 @@
+'use strict';
+const faker = require('faker');
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.bulkInsert('Users', [
+			{
+				email: 'demo@gmail.com',
+				username: 'demo',
+				hashedPassword: bcrypt.hashSync('password'),
+			},
+			{
+				email: faker.internet.email(),
+				username: 'FakeUser1',
+				hashedPassword: bcrypt.hashSync(faker.internet.password()),
+			},
+			{
+				email: faker.internet.email(),
+				username: 'FakeUser2',
+				hashedPassword: bcrypt.hashSync(faker.internet.password()),
+			},
+			{
+				email: faker.internet.email(),
+				username: 'FakeUser3',
+				hashedPassword: bcrypt.hashSync(faker.internet.password()),
+			},
+			{
+				email: faker.internet.email(),
+				username: 'FakeUser4',
+				hashedPassword: bcrypt.hashSync(faker.internet.password()),
+			},
+		]);
+	},
+
+	down: (queryInterface, Sequelize) => {
+		const Op = Sequelize.Op;
+		return queryInterface.bulkDelete('Users', {
+			username: {
+				[Op.in]: ['demo', 'FakeUser1', 'FakeUser2', 'FakeUser3', 'FakeUser4'],
+			},
+		});
+	},
+};
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,42 @@
 const router = require('express').Router();
+const sessionRouter = require('./session');
+const usersRouter = require('./users');
+const asynHandler = require('express-async-handler');
+const { User } = require('../../db/models');
+const {
+	setTokenCookie,
+	restoreUser,
+	requireAuth,
+} = require('../../utils/auth.js');
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+//--------------TESTING USER AUTHENTICATION -----------------
+// router.get(
+// 	'/set-token-cookie',
+// 	asynHandler(async (req, res) => {
+// 		const user = await User.findOne({
+// 			where: {
+// 				username: 'demo',
+// 			},
+// 		});
+// 		setTokenCookie(res, user);
+// 		return res.json({ user });
+// 	})
+// );
+
+// router.get('/restore-user', restoreUser, (req, res) => {
+// 	return res.json(req.user);
+// });
+
+// router.get('/require-auth', requireAuth, (req, res) => {
+// 	return res.json(req.user);
+// });
+//-----------------------------------------------------------
 
 router.post('/test', (req, res) => {
-	res.json({ requestBody: req.body });
+	return res.json({ requestBody: req.body });
 });
 
 module.exports = router;
